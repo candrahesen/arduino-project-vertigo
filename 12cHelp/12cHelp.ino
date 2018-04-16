05e04bdef750196383648648ca3bcbddfd73f840
Candra Hesen Parera <13515019@std.stei.itb.ac.id>
2018-04-16 21:19:52 +0700

    add i2c lcd help word

---
+++ 12cHelp/12cHelp.ino
@@ -0,0 +1,18 @@
+
+// i2c LCD Help 
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+void setup() {
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.print("HELP");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
